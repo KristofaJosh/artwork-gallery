53062d3833b4e32b5e650feb0b7958b15f027d66
kristofajosh <christopherjoshua18@live.com>
2024-03-28 17:39:55 +0100

    fix: update test

--- src/components/render-image/render-image.test.tsx
+++ src/components/render-image/render-image.test.tsx
@@ -2,13 +2,13 @@ import RenderImage from '@/components/render-image/index';
 import { render, screen } from '@testing-library/react';
 
 describe('RenderImage', () => {
-  it('applies "list" size classes by default', () => {
-    render(<RenderImage src="/test-image.jpg" alt="test" />);
+  it('give size=list: it renders correctly', () => {
+    render(<RenderImage src="/test-image.jpg" size={'list'} alt="test" />);
     expect(screen.getByTestId('render-image')).toHaveClass('h-80 w-auto');
     expect(screen.getByRole('img')).toHaveClass('object-cover');
   });
 
-  it('applies "gallery" size classes when size is set to "gallery"', () => {
+  it('given size=gallery: it renders correctly', () => {
     render(<RenderImage src="/test-image.jpg" alt="test" size="gallery" />);
     expect(screen.getByTestId('render-image')).toHaveClass('h-80 w-80');
   });
