e5a9cda73b7661d872a95049d92782adcb1c6077
kristofajosh <christopherjoshua18@live.com>
2024-03-28 14:41:06 +0100

    feat: create image builder

--- src/lib/fetchData.ts
+++ src/lib/fetchData.ts
@@ -2,15 +2,12 @@ import invariant from 'tiny-invariant';
 
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-invariant(BASE_URL, 'API BASE_URL not set; did you update your .env file?');
+invariant(BASE_URL, 'API BASE_URL not set');
 
-export async function fetchData<T>(
-  url: string,
-  init?: RequestInit,
-): Promise<T> {
-  const response = await fetch(`${BASE_URL}${url}`, init);
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-  return response.json();
+export async function fetchData<T>(url: string, init?: RequestInit): Promise<T> {
+    const response = await fetch(`${BASE_URL}${url}`, init);
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    return response.json();
 }
