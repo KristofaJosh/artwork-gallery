0623d384673d04d91990a0b965a477976825c250
kristofajosh <christopherjoshua18@live.com>
2024-03-28 14:55:00 +0100

    feat: create reusable image component

---
+++ src/components/render-image/index.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { cn } from '@/utils/cn';
+import Image from 'next/image';
+import fallbackImage from './img.png';
+
+type RenderImageType = {
+  src: string;
+  alt: string;
+  size?: 'gallery' | 'list';
+  blurDataURL?: string;
+  className?: string;
+  imgClassName?: string;
+};
+const RenderImage = ({
+  size = 'list',
+  src,
+  alt,
+  blurDataURL,
+  className,
+  imgClassName,
+}: RenderImageType) => {
+  return (
+    <div
+      className={cn(
+        'relative overflow-hidden',
+        size === 'list' ? 'h-80 w-auto' : 'h-80 w-80',
+        className,
+      )}
+    >
+      <Image
+        fill
+        src={src}
+        alt={alt}
+        blurDataURL={blurDataURL}
+        placeholder={blurDataURL ? 'blur' : 'empty'}
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        className={cn(size === 'list' && 'object-cover', imgClassName)}
+        style={{
+          backgroundImage: `url(${fallbackImage.src})`,
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          textIndent: '-9999px', // hack to hide the broken image icon
+        }}
+      />
+    </div>
+  );
+};
+
+export default RenderImage;
